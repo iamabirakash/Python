7185caac6d57c99b3e5e3fbd69352f50506ebefc
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-17 09:46:59 +0530

    UPDATED NAME

---
+++ DataTypes.py
@@ -0,0 +1,27 @@
+a=8
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a=8.7
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a='DJ'
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a=[1,2,3]
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a=(1,2,3)
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a={1:'a',2:'b'}
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a=3+10j
+print("The value of a is",a)
+print("Datatype is ",type(a))
+a={1,2,3}
+print("The value of a is",a)
+print("Datatype is ",type(a))
+
+'Rows are also known as tuples or records'
+'Coloumns are also known as Fields'
