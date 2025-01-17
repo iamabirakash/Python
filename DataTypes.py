82d05359e3c8a8eb9df06f1b933f6c4b386b4be5
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-17 10:09:17 +0530

    Add files via upload

--- DataTypes.py
+++ DataTypes.py
@@ -22,6 +22,22 @@ print("Datatype is ",type(a))
 a={1,2,3}
 print("The value of a is",a)
 print("Datatype is ",type(a))
+a=True
+print("The value of a is",a)
+print("Datatype is ",type(a))
 
 'Rows are also known as tuples or records'
 'Coloumns are also known as Fields'
+
+a=10
+b=8.7
+c='DJ'
+d=[1,2,3]
+e=(1,2,3)
+f={1:'a',2:'b'}
+g={1,2,3}
+h=True
+print(a,b,c,d,e,f,g,h)
+print(str(a))
+print(int(b))
+print(len(c))
