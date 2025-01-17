ac0e7a780812ad09a1afa7117e2f571d54c3824b
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-17 10:24:49 +0530

    Added the Arithmetic Operators

---
+++ operators.py
@@ -0,0 +1,29 @@
+'''Arithmetic Unary Comparison Bitwise Assignment
+Logical Membership Identity'''
+
+#ARITHMETIC OPERATORS
+'Addition'
+a=5
+b=3
+print("The value of a is :-",a)
+print("The value of b is :-",b)
+
+print("Addition of a and b is :-",a+b)
+
+'Substraction'
+print("Substraction of a and b is :-",a-b)
+
+'Multiplication'
+print("Multiplication of a and b is :-",a*b)
+
+'Divison'
+print("Divison of a and b is :-",a/b)
+
+'Power'
+print("Power of a and b is :-",a**b)
+
+'Floor Divison'
+print("Floor Divison of a and b is :-",a//b)
+
+'Modulus'
+print("Modulus of a and b is :-",a%b)
