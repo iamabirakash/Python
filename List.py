9b16271c95bbcf415f14404043bfb6c8fce39e10
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-01-29 09:42:57 +0530

    List Manipulation

--- List.py
+++ List.py
@@ -1,5 +1,12 @@
-a=[1,2,3,4,5]
+a=[1,2,3,4,5,6,7]
 print("The list is: ",a)
 print(a[0])
 a[0] = 6
 print(a)
+a[0] = 1
+print(a[1:4])
+print(a[0:5:2])
+print(a[::-1])
+print(a[:5])
+print(a[2:])
+print(a[6:2:-1])
